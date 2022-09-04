eac42a7a335ee2cba385f28ed18b112574bfbf99
Nguyen Trung Kien <111984477+nguyen-trung-kien-software-developer@users.noreply.github.com>
2022-09-04 14:11:48 +0700

    heroku fix

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.port || 5000;
 const path = require("path");
 
 const dbConfig = require("./config/dbConfig");
@@ -13,9 +12,10 @@ const doctorRoute = require("./routes/doctorRoute");
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
+const port = process.env.port || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/".express.static("client/build"));
+  app.use("/", express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
