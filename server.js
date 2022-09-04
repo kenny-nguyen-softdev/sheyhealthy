f3797f46c732f1e9c0df1f9f019615a9e8bcf6c4
Nguyen Trung Kien <111984477+nguyen-trung-kien-software-developer@users.noreply.github.com>
2022-09-04 14:23:07 +0700

    heroku fix

--- server.js
+++ server.js
@@ -12,13 +12,15 @@ const doctorRoute = require("./routes/doctorRoute");
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
-const port = process.env.port || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
+const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Node server started at port ${port}`));
